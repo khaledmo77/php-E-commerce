f2dd47a9fe120c8021f1dd7bcd6e448bdb52de97
khaled mohamed <73861746+khaledmo77@users.noreply.github.com>
2022-02-28 16:48:49 +0200

    Add files via upload
    Still in progress

---
+++ M-Dev-Store/index.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">    <title>Document</title>
+    <link rel="stylesheet" href="/M-Dev-Store/styles/style.css">
+</head>
+<body>
+
+<div id="top">  <!---top begin--->
+
+  <div class="container"> <!---container begin--->
+
+
+      <div class="col-md-6 offer"> <!---offer begin--->
+
+
+      <a href="" class="btn btn-success btn-sm">welcome</a>
+      <a href="checkout.php">4 Items in your cart | Total Price is :$300</a>
+
+
+
+      </div><!---offer finished--->
+
+      <div class="col-md-6"><!---col-md-6 begin--->
+                      <ul class="menu"><!---menu begin--->
+                      <li>
+                      <a href="customer_register.php">Register</a>
+                      
+                      </li>
+                      <li>
+                        <a href="checkout.php">My Account</a>
+                      </li>
+                      <li>
+                      <a href="Cart.php">Go to Cart</a>
+                      </li>
+                      <li>
+                      <a href="checkout.php">Login</a>
+                      </li>
+
+
+                      </ul><!---menu fin--->
+
+                      
+      </div><!---col-md-6 fin--->
+
+
+
+
+
+
+  </div> <!---container finished--->
+
+
+</div>   <!---top finished--->
+
+<!---navbar begin--->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+ 
+<a href="index.php" class="navbar-brand home"><!---navbar-brand home begin--->
+                          
+
+
+                          <img src="images/ecom-store-logo.png" alt="M-dev store logo" class="d-none d-sm-block"> 
+                          <img src="images/ecom-store-logo-mobile.png" alt="M-dev store logo mobile" class="d-block d-sm-none">
+
+
+
+
+
+                          </a><!---navbar-brand home finish--->
+
+  <button class="navbar-toggler m-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                           
+                            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                              <li class="nav-item active  ">
+                                <a class="nav-link " href="index.php">Home <span class="sr-only">(current)</span></a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="shop.php">Shop</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link " href="checkout.php">My Account</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link " href="cart.php">Shopping Cart</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link " href="contact.php">Contact Us</a>
+                                
+                              </li>
+                            </ul>
+                            <button class="btn btn-outline-success my-3 my-sm-0"  data-toggle="collapse" data-target="#demo"><i class="fa fa-search" style="font-size:24px"></i></button>
+      <span id="demo" class="collapse border  ">
+        <div class=" d-flex">
+        <button class="btn btn-outline-success my-3 my-sm-0"><i class="fa fa-search" style="font-size:24px"></i></button>
+        
+      <input  class=" form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search">
+      </div></span>
+                            <button class="btn btn-outline-success m-2 "><i class="fa fa-shopping-cart " style="font-size:24px"></i>4 items in your cart</button>
+                            <!-- <form class="form-inline my-2 my-lg-0"> -->
+                            
+                            
+      
+      
+    <!-- </form> -->
+
+                    
+                          </div>
+    
+
+    
+  </div>
+</nav>
+<!---navbar finished--->
+
+
+
+
+<div class="container " id="slider"><!---container begin--->
+
+<div class="col-md-12"><!---col-md-12 begin--->
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/M-Dev-Store/admin_area/slides_images/Screenshot_3.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/M-Dev-Store/admin_area/slides_images/Screenshot_4.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/M-Dev-Store/admin_area/slides_images/Screenshot_6.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="left carousel-control-prev " type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="right carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
+
+</div><!---col-md-12 finished--->
+
+</div><!---container finished--->
+
+
+
+
+<div class="advantages"><!---advantages begin--->
+
+
+    <div class="container mt-4"><!---container begin--->
+
+
+      <div class="row"><!---same-height-row begin--->
+
+
+          <div class="col-sm-4"><!---col-sn-4 begin--->
+
+            <div class="card text-center boxing"><!---box same-height begin--->
+                <div class="icon"><!---icon begin--->
+
+
+                  <i class="fa fa-heart">
+
+
+
+
+                  </i>
+
+
+
+
+
+
+                </div><!---icon finished--->
+
+
+                <h3><a href="#">Best offers</a></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga placeat alias nulla nam soluta? Laboriosa!</p>
+
+
+
+
+
+
+            </div><!---box same-heightfinished--->
+
+
+
+
+          </div><!---col-sm-4 finished--->
+
+
+          <div class="col-sm-4"><!---col-sn-4 begin--->
+
+<div class="card text-center boxing"><!---box same-height begin--->
+    <div class="icon"><!---icon begin--->
+
+
+      <i class="fa fa-tag">
+
+
+
+
+      </i>
+
+
+
+
+
+
+    </div><!---icon finished--->
+
+
+    <h3><a href="#">Best Prices</a></h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid molestias voluptatum, </p>
+
+
+
+
+
+
+</div><!---box same-heightfinished--->
+
+
+
+
+</div><!---col-sm-4 finished--->
+
+
+
+<div class="col-sm-4"><!---col-sn-4 begin--->
+
+<div class="card text-center boxing"><!---box same-height begin--->
+    <div class="icon"><!---icon begin--->
+
+
+      <i class="fa fa-thumbs-up ">
+
+
+
+
+      </i>
+
+
+
+
+
+
+    </div><!---icon finished--->
+
+
+    <h3><a href="#">100% Original Products</a></h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+
+
+
+
+
+
+</div><!---box same-heightfinished--->
+
+
+
+
+</div><!---col-sm-4 finished--->
+
+
+
+
+      </div><!---same-height-row finished--->
+
+
+
+    </div><!---container finished--->
+
+
+
+
+</div><!---advantages finished--->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
